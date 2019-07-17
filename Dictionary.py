53fed4f2bead523f90b3aa54872f93244baac51c
sbasu7241 <42107347+sbasu7241@users.noreply.github.com>
2019-07-18 01:08:25 +0530

    Using nltk

--- Dictionary.py
+++ Dictionary.py
@@ -1,34 +1,118 @@
-import json
+"""A dictionary app"""
 
-from difflib import get_close_matches
+import nltk # Natural Language Text Processing
+import pyperclip # To get clipboard data and feed it into the dictionary
+import ctypes # To pop up message box
+import time
+import re # Regular Expression might be needed
+import sys 
+from nltk.corpus import wordnet # Dictionary database access 
 
-data = json.load(open("dictionary_compact.json"))
+def stop():
+	sys.exit() # To exit the app
 
-print('\n***** WELCOME TO THE PYTHON ENGLISH DICTIONARY *****\n')
+def run(timer=2):
 
-def translate(word):
-	word = word.lower()
-	if word in data:
-		return data[word]
+	default = "NoVal"
+	
+	print('\n***** WELCOME TO THE PYTHON ENGLISH DICTIONARY *****\n')
 
-	elif word.title() in data:
-		return data[word.title()]
+	#nltk.download("wordnet") # We will be using wordnet to get meanings,antonyms and synonyms
 
-	elif word.upper() in data:
-		return data[word.upper()]
+	time_in_sec = timer * 3600
 
-	elif len(get_close_matches(word,data.keys(),1,0.6))>0:
-		y_or_n = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
-		if y_or_n[0].lower() == 'y':
-			return data[get_close_matches(word, data.keys())[0]]
-		elif y_or_n[0].lower() == 'n':
-			return "The word doesn't exist. Please double check it."
+	start_time = time.time()
 
-		else:
-			return "The word doesn't exist. Please double check it."
-	else:
-		return "The word doesn't exist. Please double check it"
-word = input("Enter word: ")
-output = translate(word)
+	ti = time.gmtime() 
 
-print("\nMeaning : " + output[0])
+	print ("Time of starting program : ",end="") 
+	print (time.asctime(ti))
+
+	while True and time.time() - start_time < time_in_sec:
+
+		word=pyperclip.paste() # Extract word from clipboard
+
+		word=re.sub('[^A-Za-z]+','', word) # Remove all non alphabet characters using re
+
+		word = word.lower()
+
+		time.sleep(1) # Pause for user to copy text
+
+		if word != default: # If word is not the old one
+			
+			print("\n* Extracted from clipboard : {}".format(word))
+
+			print("Seaching for word: {} in dictionary".format(word))
+
+			
+			define=wordnet.synsets(word) # Find synsets of word
+
+			definition = "Searching for word : " + word +"\n\n" + "Meaning : \n"
+
+			# Word meanings
+
+			ctr = 1 # Loop counter
+			
+			for j in define:
+				
+				message = str(ctr) + '. ' + j.definition() + '\n'
+				definition += "\n" + message 
+				ctr += 1
+
+
+			# Synonyms and antonyms
+
+			synonyms = []
+			antonyms = []
+
+			for j in define:
+
+				for i in j.lemmas():
+
+					synonyms.append(i.name()) # get synonyms
+					if i.antonyms():
+						antonyms.append(i.antonyms()[0].name()) # get antonyms
+
+			synonyms = set(synonyms)
+			antonyms = set(antonyms)
+
+			# Window pop up box displaying the word its meaning and antonyms and synonyms
+
+			# For windows users 
+
+			ctypes.windll.user32.MessageBoxW(0, definition+'\n\nSynonyms: '+', '.join(synonyms)+'\n\nAntonyms: '+', '.join(antonyms), 'Word Meaning - '+word, 0)
+
+			# Linux users comment out the above line and uncomment the line below
+
+			# alert(text=definitions+'\n\nSynonyms: '+', '.join(synonyms)+'\n\nAntonyms: '+', '.join(antonyms), title='Word Meaning - '+word, button='OK')
+
+			default = word
+
+
+if __name__ == "__main__":
+	run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+run()
